641d4e320dad806fc528467e3db5055b4ac61a2a
diyor <diyorbekhosilov11@gmail.com>
2024-07-06 19:06:02 +0500

    database default changed

--- config/settings.py
+++ config/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'diyerbek_moose',
+        'HOST': 'dodb-do-user-17113779-0.a.db.ondigitalocean.com',
+        'USER': 'doadmin',
+        'PASSWORD': 'AVNS_J5A-szb7Q3GiIqAXzw6',
+        'PORT': '25060'
     }
 }
 
